65958302943217c19648a75aa81875da00f2c195
layuntaFelipe <layunta.felipe@gmail.com>
2022-12-28 00:26:59 -0700

    ready-for-launch-v1

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,68 +1,100 @@
 import React, { useState } from "react";
-import video1 from "../assets/media/Andyclip16.mp4";
+import video1 from "../assets/media/Andy-Bio-01.mp4";
 import img1 from "../assets/media/16.jpg";
-import logo3 from "../assets/media/logo3.svg";
-import logo1 from "../assets/media/logo1.svg";
 import emailjs from "emailjs-com";
 import { toast } from "react-toastify";
+import bioTrackingIcon from '../icons/bio-tracking-icon.png';
+import fitnessIcon from '../icons/fitness-icon.png';
+import healthIcon from '../icons/health-icon.png';
+import longevityLivingIcon from '../icons/longevity-living-icon.png';
+import musculationIcon from '../icons/musculation-icon.png';
+import nutritionIcon from '../icons/nutrition-icon.png';
+import logoSmlSecond from '../icons/logo-sml-second.png';
+import logoBigFirst from '../icons/logo-big-first.png';
 import {
-  FaPlayCircle,
-  FaPauseCircle,
+  FaFacebookSquare,
   FaInstagram,
   FaLinkedin,
   FaTwitterSquare,
-  FaTiktok,
 } from "react-icons/fa";
-import { GiStrong, GiHealthNormal, GiLifeBar } from "react-icons/gi";
-import { IoIosNutrition, IoIosFitness } from "react-icons/io";
-import { FaHandHoldingHeart } from "react-icons/fa";
 import axios from "axios";
 
 const BenefitsItem = ({ number = 0 }) => {
+  let [openMore, setOpenMore] = useState(false);
+
+  const toggleOpen = () => {
+    // 👇️ passed function to setState
+    setOpenMore(current => !current);
+    console.log('Hey')
+  };
+
   const benefitItem = [
     {
-      image: <GiStrong style={{ fontSize: "4rem", marginRight: "1rem" }} />,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid mollitia error inventore laudantium voluptatibus laboriosam ipsa",
+      image: <img src={longevityLivingIcon} alt='bioTracking icon' style={{width: '4rem', height: '4rem', marginRight: "1rem" }} />,
+      text: "Increasing our health span is a combination of balance, physical exercise and movement, nutrition, reducing stress, meaningful, connection, mindfulness, a sense or purpose and emerging biohacking modalities.",
+      long: false,
+      longText: ''
     },
     {
       image: (
-        <GiHealthNormal style={{ fontSize: "4rem", marginRight: "1rem" }} />
+        <img src={musculationIcon} alt='bioTracking icon' style={{width: '4rem', height: '4rem', marginRight: "1rem" }} />
       ),
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid mollitia error inventore laudantium voluptatibus laboriosam ipsa",
+      text: "Muscularity and looking fit enhance your attractiveness and are positive byproducts of increased physical performance, injury prevention and reducing disease risk and/or progression.",
+      long: false,
+      longText: ''
     },
     {
-      image: <IoIosFitness style={{ fontSize: "4rem", marginRight: "1rem" }} />,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid mollitia error inventore laudantium voluptatibus laboriosam ipsa",
+      image: <img src={fitnessIcon} alt='bioTracking icon' style={{width: '4rem', height: '4rem', marginRight: "1rem" }} />,
+      text: "Exercise is the cornerstone of health and looking great.  Physical activity, resistance training and optimum protein intake is critical for men between the ages of 45 and 65.",
+      long: false,
+      longText: ''
     },
     {
       image: (
-        <IoIosNutrition style={{ fontSize: "4rem", marginRight: "1rem" }} />
+        <img src={nutritionIcon} alt='bioTracking icon' style={{width: '4rem', height: '4rem', marginRight: "1rem" }} />
       ),
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid mollitia error inventore laudantium voluptatibus laboriosam ipsa",
+      text: "Everyone has their own bio-chemical individuality.  What is optimum nutrition differs from person to person.  How much protein, carbohydrates and fat you should eat, which supplements you need and which you don’t, and how many daily calories you should consume varies wildly.",
+      long: true,
+      longText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, incidunt"
     },
     {
-      image: <GiLifeBar style={{ fontSize: "4rem", marginRight: "1rem" }} />,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid mollitia error inventore laudantium voluptatibus laboriosam ipsa",
+      image: <img src={bioTrackingIcon} alt='bioTracking icon' style={{width: '4rem', height: '4rem', marginRight: "1rem" }} />,
+      text: "Tracking your fitness and health development and implementing reliable indictors to gauge your progress is essential.  To live your healthiest and longest life possible, your physical activity levels, your diet, health history, and many other factors must be taken into account.",
+      long: true,
+      longText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, incidunt"
     },
     {
       image: (
-        <FaHandHoldingHeart style={{ fontSize: "4rem", marginRight: "1rem" }} />
+        <img src={healthIcon} alt='bioTracking icon' style={{width: '4rem', height: '4rem', marginRight: "1rem" }} />
       ),
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid mollitia error inventore laudantium voluptatibus laboriosam ipsa",
+      text: "Objective measures of health status shift in response to what you eat, how you move, how you respond to stress and the quality of your sleep.",
+      long: true,
+      longText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, incidunt"
     },
   ];
 
   return (
-    <div style={{ display: "flex", margin: "2rem 1rem" }}>
+    <div style={{ display: "flex", margin: "2rem 0rem", alignItems: 'center' }}>
       {benefitItem[number].image}
-      <p style={{ width: "100%", marginTop: "1rem" }}>
+      <p style={{ width: "100%", marginTop: "1rem", fontFamily: "'CMU Serif', sans-serif" }}>
         {benefitItem[number].text}
+        {
+          openMore.more ? (
+            benefitItem[number].longText
+          ) : ('')
+        }
+        {
+          benefitItem[number].long ? (
+            <button onClick={toggleOpen} style={{border: 'none', color: '#502c49', fontWeight: 'bold'}} >More About</button>
+          ) : ('')
+        }
       </p>
     </div>
   );
 };
 
 const Home = () => {
+
   let [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -143,7 +175,7 @@ const Home = () => {
   return (
     <div>
       <header>
-        <img src={logo1} alt="" />
+        <img src={logoBigFirst} alt="" />
       </header>
 
       <video controls>
@@ -192,68 +224,64 @@ const Home = () => {
         
         <div className="socialsContainer">
           <small>Socials</small>
-          <FaInstagram
-            style={{
-              display: "block",
-              width: "2.5rem",
-              height: "2.5rem",
-              marginBottom: "1rem",
-              marginTop: "0.5rem",
-            }}
-          />
-          <FaLinkedin
-            style={{
-              display: "block",
-              width: "2.5rem",
-              height: "2.5rem",
-              marginBottom: "1rem",
-            }}
-          />
-          <FaTwitterSquare
-            style={{ width: "2.5rem", height: "2.5rem", marginBottom: "1rem" }}
-          />
-          <FaTiktok
-            style={{ width: "2.5rem", height: "2.5rem", marginBottom: "1rem" }}
-          />
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+            <FaInstagram
+              style={{
+                display: "block",
+                width: "2.5rem",
+                height: "2.5rem",
+                marginBottom: "1rem",
+                marginTop: "0.5rem",
+              }}
+            />
+          </a>
+          <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin
+              style={{
+                display: "block",
+                width: "2.5rem",
+                height: "2.5rem",
+                marginBottom: "1rem",
+              }}
+            />
+          </a>
+          <a href="https://www.twitter.com/" target="_blank" rel="noopener noreferrer">
+            <FaTwitterSquare
+              style={{ width: "2.5rem", height: "2.5rem", marginBottom: "1rem" }}
+            />
+          </a>
+          <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+            <FaFacebookSquare
+              style={{ width: "2.5rem", height: "2.5rem", marginBottom: "1rem" }}
+            />
+          </a>
         </div>
       </div>
 
       <div className="bio">
-        <img src={img1} alt="" />
+        <img src={img1} style={{borderRadius: '50%'}} alt="" />
         <div className="bioInfo">
           <h2>Andrew's Bio</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non saepe
-            architecto nisi illo quam recusandae ipsam quibusdam labore
-            molestias atque tempora totam, animi est! Eius ratione adipisci
-            voluptatibus earum dolorumLorem ipsum dolor sit amet consectetur
-            adipisicing elit. Non saepe architecto nisi illo quam recusandae
-            ipsam quibusdam labore molestias atque tempora totam, animi est!
-            Eius ratione adipisci voluptatibus earum dolorum.
+          Andrew Kolasko is the CEO and founder of Andrew Kolasko Life Center. He is a visionary health leader and former champion natural bodybuilder with more than 40 years of weight training, athletic performance and sound eating and nutritional practices. Andrew has achieved the rare air balance of combining the right amount of real world fitness education with longevity based emerging sciences to implement healthy processes and the proven methodologies to attain the look, performance and body mechanics and metabolic health of men several years younger than his chronological age of 55.
           </p>
           <br />
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non saepe
-            architecto nisi illo quam recusandae ipsam quibusdam labore
-            molestias atque tempora totam, animi est! Eius ratione adipisci
-            voluptatibus earum dolorumLorem ipsum dolor sit amet consectetur
-            adipisicing elit. Non saepe architecto nisi illo quam recusandae
-            ipsam quibusdam labore molestias atque tempora totam, animi est!
-            Eius ratione adipisci voluptatibus earum dolorum.
+          Andrew uses his own healthy practices, nutritional knowledge and novel training methods with his clients to enhance their musculation, strength, vitality, appearance and improve lifespan.
           </p>
           <br />
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non saepe
-            architecto nisi illo quam recusandae ipsam quibusdam labore
-            molestias atque tempora totam, animi est! Eius ratione adipisci
-            voluptatibus earum dolorumLorem ipsum dolor sit amet consectetur
-            adipisicing elit.
+          Andrew’s specialization is working with business owners and corporate executives who have put all their energy and time into building their companies, while sacrificing their own physical well being.  At this later stage of life, enjoying the fruits of their labour can be challenging because of physical and health limitations, but turning back time is not impossible. 
+          </p>
+          <br/>
+          <p>
+          Andrew’s skills and passion for his craft has the power to change the way men will live their lives in their later years by sharing his expertise to adjust metabolic health and fitness to a level that will make men over 45, feel more confident and look years younger.
           </p>
         </div>
       </div>
 
       <div className="benefits">
-        <h2>Benefits of AKLC</h2>
+        <h2>Benefits of Andrew Kolasko Life Center</h2>
         <div className="benefitsItems">
           <div className="leftUl">
             <ul>
@@ -291,50 +319,53 @@ const Home = () => {
               <label>Register for Webinar</label>
               <input type="text" placeholder="Name" name="name" id="name" />
               <input type="email" placeholder="Email" name="email" id="email" />
-              <button>Subscribe</button>
+              <button>Register</button>
             </form>
           </div>
         </div>
         <div className="socialsContainer">
           <small>Socials</small>
-          <FaInstagram
-            style={{
-              display: "block",
-              width: "2.5rem",
-              height: "2.5rem",
-              marginBottom: "1rem",
-              marginTop: "0.5rem",
-            }}
-          />
-          <FaLinkedin
-            style={{
-              display: "block",
-              width: "2.5rem",
-              height: "2.5rem",
-              marginBottom: "1rem",
-            }}
-          />
-          <FaTwitterSquare
-            style={{ width: "2.5rem", height: "2.5rem", marginBottom: "1rem" }}
-          />
-          <FaTiktok
-            style={{ width: "2.5rem", height: "2.5rem", marginBottom: "1rem" }}
-          />
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+            <FaInstagram
+              style={{
+                display: "block",
+                width: "2.5rem",
+                height: "2.5rem",
+                marginBottom: "1rem",
+                marginTop: "0.5rem",
+              }}
+            />
+          </a>
+          <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin
+              style={{
+                display: "block",
+                width: "2.5rem",
+                height: "2.5rem",
+                marginBottom: "1rem",
+              }}
+            />
+          </a>
+          <a href="https://www.twitter.com/" target="_blank" rel="noopener noreferrer">
+            <FaTwitterSquare
+              style={{ width: "2.5rem", height: "2.5rem", marginBottom: "1rem" }}
+            />
+          </a>
+          <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+            <FaFacebookSquare
+              style={{ width: "2.5rem", height: "2.5rem", marginBottom: "1rem" }}
+            />
+          </a>
         </div>
       </div>
 
-      <div style={{ overflow: "auto", padding: "2rem" }}>
-        <img style={{ float: "left", overflow: "auto" }} src={logo3} alt="" />
+      <div className="footer">
+        <img src={logoSmlSecond} alt="" />
         <div className="footerMoreInfoRight">
-          <p style={{ marginBottom: "3rem" }}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Necessitatibus tenetur qui nobis, at veniam atque quaerat sapiente,
-            harum facere odit voluptate delectus in dolorum obcaecati nemo
-            assumenda pariatur soluta nihil nesciunt et. Vero exercitationem
-            voluptate ratione fugit nostrum, voluptatum corporis aliquid saepe!
-            Ipsum illum consequuntur modi eveniet voluptates! Nobis, molestiae.
+          <p>
+            This website is for informational purposes only. It does not constitute the practice of medicine, nursing or other professional health care services, including the giving of medical advice. No doctor/patient relationship is formed. The use of the information contained within this website and any video, text, graphics, images and/or other materials linked to this website is at the users own risk. The content of this website is not intended to be a substitute for professional medical advice, diagnosis and/or treatment. Consult with your doctor before taking any medications, vitamins, herbs and/or supplements of any kind. Users should not disregard or delay in obtaining medical advice for any medical condition they have and they should seek the assistance of their qualified health care professionals for any such conditions.
           </p>
-          <small>© Andrew Kolasko Life Center</small>
+          <small style={{position: 'absolute', right: '3rem'}}>© 2022 Andrew Kolasko Life Center</small>
         </div>
       </div>
     </div>
